3316dfd12292ef176a13b944512dbf8872d0fa87
Malanchenko Sergii <malan.sergii@gmail.com>
2024-03-18 16:04:52 +0200

    change with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,61 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactsForm from './contactsForm';
 import Filter from './filter';
 import ContactList from './contactList';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount = () => {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const exist = localStorage.getItem('contacts');
+    console.log(exist);
+    if (exist) {
+      return JSON.parse(exist);
     }
+    return [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onFilterChange = e => {
+    setFilter(e.target.value);
   };
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  onFilterChange = e => {
-    this.setState({
-      filter: e.target.value,
-    });
-  };
-
-  createContact = data => {
-    const exist = [...this.state.contacts].some(item => item.name === data.name);
+  const createContact = data => {
+    const exist = [...contacts].some(item => item.name === data.name);
     if (!exist) {
-      this.setState(prev => ({
-        contacts: [...prev.contacts, { id: nanoid(), ...data }],
-      }));
+      setContacts([...contacts, { id: nanoid(), ...data }]);
     } else {
       alert(`${data.name} is already in contacts`);
     }
   };
 
-  removeContact = e => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== e.target.id),
-    }));
+  const removeContact = e => {
+    setContacts(contacts.filter(contact => contact.id !== e.target.id));
   };
 
-  render() {
-    const filterNormalized = this.state.filter.toLowerCase();
-    const filtered = [...this.state.contacts].filter(contact =>
-      contact.name.toLowerCase().includes(filterNormalized)
-    );
+  const filterNormalized = filter.toLowerCase();
+  const filtered = [...contacts].filter(contact =>
+    contact.name.toLowerCase().includes(filterNormalized)
+  );
 
-    return (
-      <>
-        <h2>Phonebook</h2>
-        <ContactsForm getData={this.createContact}></ContactsForm>
-
-        <h2>Contacts</h2>
-        <Filter onFilterChange={this.onFilterChange} filter={this.state.filter}></Filter>
-        <ContactList filtered={filtered} removeContact={this.removeContact} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <ContactsForm getData={createContact}></ContactsForm>
+      {contacts.length > 0 ? (
+        <>
+          <h2>Contacts</h2>
+          <Filter onFilterChange={onFilterChange} filter={filter}></Filter>
+          <ContactList filtered={filtered} removeContact={removeContact} />
+        </>
+      ) : (
+        <p>No contacts in your book yet</p>
+      )}
+    </>
+  );
+};
 
 export default App;
--- src/components/contactsForm/ContactsForm.js
+++ src/components/contactsForm/ContactsForm.js
@@ -1,39 +1,28 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Formik, Form, Field } from 'formik';
 
-class ContactsForm extends Component {
-  state = {
-    name: '',
-    number: '',
+const ContactsForm = ({ getData }) => {
+  const onFormikSubmit = (values, actions) => {
+    getData(values);
+    actions.resetForm();
   };
 
-  render() {
-    const onFormikSubmit = (values, actions) => {
-      this.props.getData(values);
-      actions.resetForm();
-    };
-
-    return (
-      <Formik
-        initialValues={{ name: '', number: '' }}
-        onSubmit={onFormikSubmit}
-      >
-        <Form>
-          <label>
-            Name
-            <Field type="text" name="name" required />
-          </label>
-          <label>
-            phone
-            <Field type="tel" name="number" required />
-          </label>
-          <button type="submit">Add Contact</button>
-        </Form>
-      </Formik>
-    );
-  }
-}
+  return (
+    <Formik initialValues={{ name: '', number: '' }} onSubmit={onFormikSubmit}>
+      <Form>
+        <label>
+          Name
+          <Field type="text" name="name" required />
+        </label>
+        <label>
+          phone
+          <Field type="tel" name="number" required />
+        </label>
+        <button type="submit">Add Contact</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default ContactsForm;
 
